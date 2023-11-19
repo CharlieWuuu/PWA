3fea321a1fff4ceb0e8d41ed535b4e109521e8dd
CharlieWuuu <charliewu500@gmail.com>
2023-11-19 15:31:39 +0800

    service0worker

--- service-worker.js
+++ service-worker.js
@@ -7,3 +7,22 @@ workbox.routing.registerRoute(({ request }) => {
 
 console.log(123);
 console.log(workbox);
+
+var CACHE_NAME = 'task-management';
+var urlsToCache = [];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }),
+  );
+});
