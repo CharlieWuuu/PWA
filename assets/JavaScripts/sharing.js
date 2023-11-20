f9d5cb9b6278479aeba82670ad48481f8758adca
CL Wu <CharlieWu500@gmail.com>
2023-11-20 13:38:39 +0800

    分享功能

--- assets/JavaScripts/sharing.js
+++ assets/JavaScripts/sharing.js
@@ -4,9 +4,26 @@ shareBtn.onclick = async (filesArray) => {
     console.log(123);
     if (navigator.canShare) {
         navigator.share({
-            files: filesArray,
+            url: 'https://charliewuuu.github.io/PWA/',
             title: 'PWA 超酷！',
             text: '我學會怎麼建立一個 PWA 程式了！',
         });
     }
 };
+
+// "share_target": {
+// 	"action": "/share-photo",
+// 	"method": "POST",
+// 	"enctype": "multipart/form-data",
+// 	"params": {
+// 			"title": "name",
+// 			"text": "description",
+// 			"url": "link",
+// 			"files": [
+// 					{
+// 							"names": "photos",
+// 							"accept": "image/png"
+// 					}
+// 			]
+// 	}
+// },
