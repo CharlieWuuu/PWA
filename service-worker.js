6d4158032a556c9247c5a8fca1556efb078bd3ab
CharlieWuRichi <charliewu@richitech.com.tw>
2023-11-21 17:11:43 +0800

    測試另一種快取設定方式

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,73 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.js');
-workbox.routing.registerRoute(new RegExp('https://unpkg.com/vue@3/dist/vue.global.js'), new workbox.strategies.CacheFirst());
+// importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.js');
+// workbox.routing.registerRoute(new RegExp('https://unpkg.com/vue@3/dist/vue.global.js'), new workbox.strategies.CacheFirst());
 
-var CACHE_NAME = 'PWA';
+// var CACHE_NAME = 'PWA';
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(['/', './index.html', './manifest.json']);
-        }),
-    );
-});
+// self.addEventListener('install', (e) => {
+//     e.waitUntil(
+//         caches.open(cacheName).then((cache) => {
+//             return cache.addAll(['/', './index.html', './manifest.json']);
+//         }),
+//     );
+// });
+
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches
+//             .open(cacheName)
+//             .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+//             .then((response) => {
+//                 return response || fetch(event.request);
+//             }),
+//     );
+// });
+
+// service-worker.js
+
+const putInCache = async (request, response) => {
+    const cache = await caches.open('v1');
+    await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }) => {
+    // First try to get the resource from the cache.
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+
+    // If the response was not found in the cache,
+    // try to get the resource from the network.
+    try {
+        const responseFromNetwork = await fetch(request);
+        // If the network request succeeded, clone the response:
+        // - put one copy in the cache, for the next time
+        // - return the original to the app
+        // Cloning is needed because a response can only be consumed once.
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        // If the network request failed,
+        // get the fallback response from the cache.
+        const fallbackResponse = await caches.match(fallbackUrl);
+        if (fallbackResponse) {
+            return fallbackResponse;
+        }
+        // When even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object.
+        return new Response('Network error happened', {
+            status: 408,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+};
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches
-            .open(cacheName)
-            .then((cache) => cache.match(event.request, { ignoreSearch: true }))
-            .then((response) => {
-                return response || fetch(event.request);
-            }),
+        cacheFirst({
+            request: event.request,
+            fallbackUrl: '/index.html',
+        }),
     );
 });
