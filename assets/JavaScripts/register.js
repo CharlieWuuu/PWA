3d9fa7d7876c116cf33734380f2471caa5210fbc
CL Wu <CharlieWu500@gmail.com>
2023-11-21 23:22:44 +0800

    調整register.js程式

--- assets/JavaScripts/register.js
+++ assets/JavaScripts/register.js
@@ -2,13 +2,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
             .register('./service-worker.js')
-            .then((registration) => {
-                console.log('Service worker registration succeeded:', registration);
-                // At this point, you can optionally do something
-                // with registration. See https://developer.mozilla.org/zh-CN/docs/Web/API/ServiceWorkerRegistration
-            })
-            .catch((error) => {
-                console.error(`Service worker registration failed: ${error}`);
-            });
+            .then((registration) => console.log('Service worker registration succeeded:', registration))
+            .catch((error) => console.error(`Service worker registration failed: ${error}`));
     });
 }
