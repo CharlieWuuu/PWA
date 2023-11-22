ca8c0489ab090287eb3f54dc1570e283ff6500cb
CharlieWuRichi <charliewu@richitech.com.tw>
2023-11-22 14:42:10 +0800

    SW加入逐行註解、修改網站狀態顯示

--- assets/JavaScripts/register.js
+++ assets/JavaScripts/register.js
@@ -8,7 +8,17 @@ if ('serviceWorker' in navigator) {
                 const sw = registration.active || registration.installing || registration.waiting;
 
                 // 向 Service Worker 發送消息
-                sw.postMessage({ type: 'log', message: 'SW已安裝' });
+                sw.postMessage({ type: 'sw', message: 'Service Worker 已安裝' });
+
+                // 傳遞 SW 已安裝的訊息到前端
+                navigator.serviceWorker.addEventListener('message', (event) => {
+                    if (event.data && event.data.type === 'sw') {
+                        console.log('發自register.js：' + event.data.message);
+
+                        // 如果你想將消息顯示在 DOM 中，可以使用以下方式：
+                        document.getElementById('sw').innerText = event.data.message;
+                    }
+                });
             })
             .catch((error) => {
                 console.error('Service Worker registration failed:', error);
--- service-worker.js
+++ service-worker.js
@@ -1,88 +1,27 @@
+// 引入 Workbox，一個用於簡化 Service Worker 開發的函式庫
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.js');
 
+// 註冊 Service Worker 路由，使用 Workbox 的 registerRoute 方法，需要帶入兩個參數
 workbox.routing.registerRoute(
-    new RegExp('.*'), // 匹配所有 URL
-    new workbox.strategies.NetworkFirst(),
+    new RegExp('.*'), // 使用正規表達式匹配所有 URL，.* 表示所有字符零次或多次
+    new workbox.strategies.NetworkFirst(), // 使用 NetworkFirst 快取策略，優先嘗試從網路獲取資源
 );
 
+// 用 Service Worker 傳訊息給前端（Service Worker 不能直接操作 DOM）
+// 監聽 Service Worker 收到的訊息事件
 self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'log') {
+    // 檢查事件中是否包含數據，以及數據類型是否為 'sw'
+    if (event.data && event.data.type === 'sw') {
+        // 使用 self.clients.matchAll() 取得所有已連接的 clients（前端頁面）
         self.clients.matchAll().then((clients) => {
+            // 遍歷所有 clients
             clients.forEach((client) => {
-                client.postMessage({ type: 'log', message: event.data.message });
-                console.log(event.data.message);
+                // 向每個 client 傳送包含 'sw' 類型和特定消息的訊息
+                client.postMessage({ type: 'sw', message: event.data.message });
+
+                // 在 Service Worker 控制台打印相同的消息
+                console.log('發自service-worker.js：' + event.data.message);
             });
         });
     }
 });
-
-// var CACHE_NAME = 'PWA';
-
-// self.addEventListener('install', (e) => {
-//     e.waitUntil(
-//         caches.open(cacheName).then((cache) => {
-//             return cache.addAll(['/', './index.html', './manifest.json']);
-//         }),
-//     );
-// });
-
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches
-//             .open(cacheName)
-//             .then((cache) => cache.match(event.request, { ignoreSearch: true }))
-//             .then((response) => {
-//                 return response || fetch(event.request);
-//             }),
-//     );
-// });
-
-// service-worker.js
-
-// const putInCache = async (request, response) => {
-//     const cache = await caches.open('v1');
-//     await cache.put(request, response);
-// };
-
-// const cacheFirst = async ({ request, fallbackUrl }) => {
-//     // First try to get the resource from the cache.
-//     const responseFromCache = await caches.match(request);
-//     if (responseFromCache) {
-//         return responseFromCache;
-//     }
-
-//     // If the response was not found in the cache,
-//     // try to get the resource from the network.
-//     try {
-//         const responseFromNetwork = await fetch(request);
-//         // If the network request succeeded, clone the response:
-//         // - put one copy in the cache, for the next time
-//         // - return the original to the app
-//         // Cloning is needed because a response can only be consumed once.
-//         putInCache(request, responseFromNetwork.clone());
-//         return responseFromNetwork;
-//     } catch (error) {
-//         // If the network request failed,
-//         // get the fallback response from the cache.
-//         const fallbackResponse = await caches.match(fallbackUrl);
-//         if (fallbackResponse) {
-//             return fallbackResponse;
-//         }
-//         // When even the fallback response is not available,
-//         // there is nothing we can do, but we must always
-//         // return a Response object.
-//         return new Response('Network error happened', {
-//             status: 408,
-//             headers: { 'Content-Type': 'text/plain' },
-//         });
-//     }
-// };
-
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         cacheFirst({
-//             request: event.request,
-//             fallbackUrl: '/index.html',
-//         }),
-//     );
-// });
